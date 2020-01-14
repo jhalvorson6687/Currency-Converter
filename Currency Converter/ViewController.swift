751539b2b5415f4d8dc7385bbe7b251571419c0f
Jacob Halvorson <jhalvorson6687@icloud.com>
2020-01-13 20:25:26 -0800

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Jacob Halvorson on 1/13/20.
+//  Copyright © 2020 Jacob Halvorson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
